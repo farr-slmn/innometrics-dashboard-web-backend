57e1e6409cdcb08b573eff26ce2555257390968e
Valiullin Albert <albert-val.95@mail.ru>
2017-10-29 15:59:22 +0300

    admin panel configuration

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,13 @@ class Project(models.Model):
     name = models.CharField(max_length=150, blank=True)
     description = models.CharField(max_length=150, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class UserParticipation(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.user) + " - " + str(self.project)
