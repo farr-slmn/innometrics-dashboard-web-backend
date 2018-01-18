41b5282fdbebef1d9d8322c1724270d0b876ce5b
Valiullin Albert <albert-val.95@mail.ru>
2018-01-19 00:05:41 +0300

    tests: raw metric filtering, raw metric data

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,5 +1,3 @@
-from unittest import skip
-
 from django.contrib.auth.models import User
 from django.test import TransactionTestCase
 from rest_framework.utils import json
@@ -95,32 +93,34 @@ class MetricTestCase(TransactionTestCase):
                 "filters": {}
             }
         }
+        new_metric_response = {
+            "info": {
+                "field": "m_begin",
+                "filters": {}
+            },
+            "id": 1, "participation": 1,
+            "name": "metric_1",
+            "fields": ["extra_field", "m_begin", "m_end"],
+            "type": "R"
+        }
+
+        response = self.client.put('/projects/metrics/', content_type='application/json', data=json.dumps(metric_json))
+        self.assertEqual(response.status_code, 201)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), new_metric_response)
+
         metrics_response = {
             "metrics": [
                 {
-                    "measurements": [
-                        {"id": 1, "name": "m_begin", "value": "123", "type": "long", "activity_id": 1,
-                         "entity": "activity_1", "group": None},
-                        {"id": 4, "name": "m_begin", "value": "234", "type": "long", "activity_id": 2,
-                         "entity": "activity_2", "group": None},
-                        {"id": 6, "name": "m_begin", "value": "567", "type": "long", "activity_id": 3,
-                         "entity": "activity_1", "group": None}
-                    ],
                     "info": {
                         "field": "m_begin",
                         "filters": {}
                     },
                     "id": 1, "participation": 1,
                     "name": "metric_1",
-                    "fields": ["extra_field", "m_begin", "m_end"],
                     "type": "R"
                 }
             ]
         }
-
-        response = self.client.put('/projects/metrics/', content_type='application/json', data=json.dumps(metric_json))
-        self.assertEqual(response.status_code, 201)
-        self.assertJSONEqual(str(response.content, encoding='utf8'), metrics_response)
 
         response = self.client.get("/projects/metrics/")
         self.assertEqual(response.status_code, 200)
@@ -306,7 +306,7 @@ class RawMetricTestCase(TransactionTestCase):
                     "measurements": [
                         {"name": "m_begin", "value": "567", "type": "long"},
                         {"name": "m_end", "value": "890", "type": "long"},
-                        {"name": "extra_field", "value": "example_2", "type": "string"},
+                        {"name": "another_extra_field", "value": "example_2", "type": "string"},
                     ]
                 },
             ]
@@ -341,25 +341,36 @@ class RawMetricTestCase(TransactionTestCase):
                 "filters": {}
             }
         }
-        metrics_response = {
-            "metrics": [
-                {
-                    "fields": ["extra_field", "m_begin", "m_end"],
-                    "measurements": [],
-                    "info": {
-                        "field": "does not exist",
-                        "filters": {}
-                    },
-                    "id": 1, "participation": 1,
-                    "name": "metric_1",
-                    "type": "R"
-                }
-            ]
+        metric_response = {
+            "fields": ["another_extra_field", "extra_field", "m_begin", "m_end"],
+            "info": {
+                "field": "does not exist",
+                "filters": {}
+            },
+            "id": 1, "participation": 1,
+            "name": "metric_1",
+            "type": "R"
         }
 
         response = self.client.put('/projects/metrics/', content_type='application/json', data=json.dumps(metric_json))
         self.assertEqual(response.status_code, 201)
-        self.assertJSONEqual(str(response.content, encoding='utf8'), metrics_response)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), metric_response)
+
+        metric_data_response = {
+            "fields": ["another_extra_field", "extra_field", "m_begin", "m_end"],
+            "measurements": [],
+            "info": {
+                "field": "does not exist",
+                "filters": {}
+            },
+            "id": 1, "participation": 1,
+            "name": "metric_1",
+            "type": "R"
+        }
+
+        response = self.client.get("/projects/metrics/1/data/")
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), metric_data_response)
 
     def test_empty_filter_value(self):
         metric_json = {
@@ -392,29 +403,39 @@ class RawMetricTestCase(TransactionTestCase):
                 "filters": {}
             }
         }
-        metrics_response = {
-            "metrics": [
-                {
-                    "fields": [],
-                    "measurements": [],
-                    "info": {
-                        "activity": "does not exist",
-                        "field": "m_begin",
-                        "filters": {}
-                    },
-                    "id": 1, "participation": 1,
-                    "name": "metric_1",
-                    "type": "R"
-                }
-            ]
+        metric_response = {
+            "fields": [],
+            "info": {
+                "activity": "does not exist",
+                "field": "m_begin",
+                "filters": {}
+            },
+            "id": 1, "participation": 1,
+            "name": "metric_1",
+            "type": "R"
         }
 
         response = self.client.put('/projects/metrics/', content_type='application/json', data=json.dumps(metric_json))
         self.assertEqual(response.status_code, 201)
-        self.assertJSONEqual(str(response.content, encoding='utf8'), metrics_response)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), metric_response)
 
-    # TODO fix test
-    @skip
+        metric_data_response = {
+            "fields": [],
+            "measurements": [],
+            "info": {
+                "activity": "does not exist",
+                "field": "m_begin",
+                "filters": {}
+            },
+            "id": 1, "participation": 1,
+            "name": "metric_1",
+            "type": "R"
+        }
+
+        response = self.client.get("/projects/metrics/1/data/")
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), metric_data_response)
+
     def test_value_filter(self):
         self.maxDiff = None
         metric_json = {
@@ -427,30 +448,40 @@ class RawMetricTestCase(TransactionTestCase):
                 }
             }
         }
-        metrics_response = {
-            "metrics": [
-                {
-                    "fields": ["extra_field", "m_begin", "m_end"],
-                    "measurements": [
-                        {"id": 4, "name": "m_begin", "value": "234", "type": "long", "activity_id": 2,
-                         "entity": "activity_2", "group": None},
-                        {"id": 6, "name": "m_begin", "value": "567", "type": "long", "activity_id": 3,
-                         "entity": "activity_1", "group": None}
-                    ],
-                    "info": {
-                        "field": "m_begin",
-                        "filters": {"field_from": "234"}
-                    },
-                    "id": 1, "participation": 1,
-                    "name": "metric_1",
-                    "type": "R"
-                }
-            ]
+        new_metric_response = {
+            "fields": ["another_extra_field", "extra_field", "m_begin", "m_end"],
+            "info": {
+                "field": "m_begin",
+                "filters": {"field_from": "234"}
+            },
+            "id": 1, "participation": 1,
+            "name": "metric_1",
+            "type": "R"
         }
 
         response = self.client.put('/projects/metrics/', content_type='application/json', data=json.dumps(metric_json))
         self.assertEqual(response.status_code, 201)
-        self.assertJSONEqual(str(response.content, encoding='utf8'), metrics_response)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), new_metric_response)
+
+        metric_data_response = {
+            "fields": ["another_extra_field", "extra_field", "m_begin", "m_end"],
+            "measurements": [
+                {"id": 4, "name": "m_begin", "value": "234", "type": "long", "activity_id": 2,
+                 "entity": "activity_2", "group": None},
+                {"id": 6, "name": "m_begin", "value": "567", "type": "long", "activity_id": 3,
+                 "entity": "activity_1", "group": None}
+            ],
+            "info": {
+                "field": "m_begin",
+                "filters": {"field_from": "234"}
+            },
+            "id": 1, "participation": 1,
+            "name": "metric_1",
+            "type": "R"
+        }
+        response = self.client.get("/projects/metrics/1/data/")
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), metric_data_response)
 
         metric_json = {
             "name": "metric_2",
@@ -462,27 +493,107 @@ class RawMetricTestCase(TransactionTestCase):
                 }
             }
         }
-        metrics_response = {
+        new_metric_response = {
+            "fields": ["another_extra_field", "extra_field", "m_begin", "m_end"],
+            "info": {
+                "field": "m_begin",
+                "filters": {"field_to": "234"}
+            },
+            "id": 2, "participation": 1,
+            "name": "metric_2",
+            "type": "R"
+        }
+
+        response = self.client.put('/projects/metrics/', content_type='application/json', data=json.dumps(metric_json))
+        self.assertEqual(response.status_code, 201)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), new_metric_response)
+
+        metric_data_response = {
+            "fields": ["another_extra_field", "extra_field", "m_begin", "m_end"],
+            "measurements": [
+                {"id": 1, "name": "m_begin", "value": "123", "type": "long", "activity_id": 1,
+                 "entity": "activity_1", "group": None},
+                {"id": 4, "name": "m_begin", "value": "234", "type": "long", "activity_id": 2,
+                 "entity": "activity_2", "group": None},
+            ],
+            "info": {
+                "field": "m_begin",
+                "filters": {"field_to": "234"}
+            },
+            "id": 2, "participation": 1,
+            "name": "metric_2",
+            "type": "R"
+        }
+        response = self.client.get("/projects/metrics/2/data/")
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), metric_data_response)
+
+    def test_metric_value(self):
+        self.client.login(username='test_user_1', password='test_password')
+        metric_json = {
+            "name": "metric_1",
+            "type": "R",
+            "info": {
+                "field": "m_begin",
+                "filters": {}
+            }
+        }
+        response = self.client.put('/projects/metrics/', content_type='application/json', data=json.dumps(metric_json))
+        self.assertEqual(response.status_code, 201)
+
+        metrics_values_response = {
             "metrics": [
                 {
-                    "fields": ["extra_field", "m_begin", "m_end"],
-                    "measurements": [
-                        {"id": 1, "name": "m_begin", "value": "123", "type": "long", "activity_id": 1,
-                         "entity": "activity_1", "group": None},
-                        {"id": 4, "name": "m_begin", "value": "234", "type": "long", "activity_id": 2,
-                         "entity": "activity_2", "group": None},
-                    ],
                     "info": {
                         "field": "m_begin",
-                        "filters": {"field_to": "234"}
+                        "filters": {}
                     },
-                    "id": 2, "participation": 1,
-                    "name": "metric_2",
+                    "id": 1, "participation": 1,
+                    "name": "metric_1",
+                    "fields": ["another_extra_field", "extra_field", "m_begin", "m_end"],
                     "type": "R"
                 }
             ]
         }
 
-        response = self.client.put('/projects/metrics/', content_type='application/json', data=json.dumps(metric_json))
+        response = self.client.get("/projects/metrics/values/")
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), metrics_values_response)
+
+    def test_metric_data(self):
+        self.client.login(username='test_user_1', password='test_password')
+
+        metric_json = {
+            "name": "metric_1",
+            "type": "R",
+            "info": {
+                "field": "m_begin",
+                "filters": {}
+            }
+        }
+        response = self.client.put('/projects/metrics/', content_type='application/json',
+                                   data=json.dumps(metric_json))
         self.assertEqual(response.status_code, 201)
-        self.assertJSONEqual(str(response.content, encoding='utf8'), metrics_response)
+
+        metric_data_response = {
+            "measurements": [
+                {"id": 1, "name": "m_begin", "value": "123", "type": "long", "activity_id": 1,
+                 "entity": "activity_1", "group": None},
+                {"id": 4, "name": "m_begin", "value": "234", "type": "long", "activity_id": 2,
+                 "entity": "activity_2", "group": None},
+                {"id": 6, "name": "m_begin", "value": "567", "type": "long", "activity_id": 3,
+                 "entity": "activity_1", "group": None}
+            ],
+            "info": {
+                "field": "m_begin",
+                "filters": {}
+            },
+            "id": 1, "participation": 1,
+            "name": "metric_1",
+            "fields": ["another_extra_field", "extra_field", "m_begin", "m_end"],
+            "type": "R"
+        }
+
+        response = self.client.get("/projects/metrics/1/data/")
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), metric_data_response)
