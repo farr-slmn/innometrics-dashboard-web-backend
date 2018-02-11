0ed0ba6fb39193d33c17ca01541d01772002064c
Valiullin Albert <albert-val.95@mail.ru>
2018-02-11 20:45:05 +0300

    backend: fix retrieving metrics current values, refactoring

--- projects/services.py
+++ projects/services.py
@@ -13,6 +13,8 @@ from activities.models import Entity
 from measurements.models import Measurement
 from projects.models import Metric
 
+DAY_SEC = 24 * 60 * 60
+
 
 def retrieve_metrics(participation):
     result = []
@@ -39,39 +41,72 @@ def retrieve_metric_data(metric, participation, result_list):
         metric_data = model_to_dict(metric)
 
     if metric.type == Metric.RAW:
-        retrieve_raw_metric_data(metric, participation, metric_data)
+        metric_data['measurements'], metric_data['fields'] = retrieve_raw_metric_data(metric, participation)
     else:
         retrieve_composite_metric_data(metric, participation, metric_data, result_list)
     return metric_data
 
 
-def retrieve_raw_metric_data(metric, participation, metric_data):
-    measurements = raw_metric_filters_qs(metric, participation, metric_data)
-    measurements = measurements.filter(name=metric.info['field']) \
+# for Raw metric
+def retrieve_raw_metric_data(metric, participation):
+    """
+    Retrieves measurements and corresponding activity fields from provided raw metric
+
+    :param metric: Metric model instance
+    :param participation: Participation model instance
+    :return: (measurements, fields) tuple of
+    list of measurement (dict with keys: 'id', 'name', 'value', 'type', 'activity_id', 'entity', 'group')
+    and  list of fields (dict with keys: 'name', 'type')
+    """
+    if metric.type != Metric.RAW:
+        raise ValueError
+
+    measurements_qs, fields_qs = raw_metric_filters_qs(metric, participation)
+    measurements_qs = measurements_qs.filter(name=metric.info['field']) \
         .annotate(entity=F('activity__entity__name'), group=F('activity__entity__group__name')) \
         .values('id', 'name', 'value', 'type', 'activity_id', 'entity', "group") \
         .order_by('id')
-    metric_data['measurements'] = list(measurements)
+    return list(measurements_qs), list(fields_qs)
 
 
-def raw_metric_filters_qs(metric, participation, metric_data):
-    measurements = Measurement.objects.filter(activity__participation=participation)
-    # measurements = Measurement.objects.filter(name=measurement_field, activity__participation=participation)
+# for Raw metric
+def raw_metric_filters_qs(metric, participation):
+    """
+    Builds QuerySets for measurements and corresponding activity fields from metric info.
+
+    :param metric: Metric model instance
+    :param participation: UserParticipation model instance
+    :return: (measurements_qs, metric_data_fields_qs) tuple of measurements QuerySet and metric data fields QuerySet
+    """
+    if metric.type != Metric.RAW:
+        raise ValueError
+
+    measurements_qs = Measurement.objects.filter(activity__participation=participation)
 
     activity_name = metric.info.get('activity', None)
     if activity_name:
-        measurements = measurements.filter(activity__entity__name=activity_name)
+        measurements_qs = measurements_qs.filter(activity__entity__name=activity_name)
 
-    measurements = apply_filters(measurements, metric.info['filters'], metric.info['field'])
+        measurements_qs = apply_filters(measurements_qs, metric.info['filters'], metric.info['field'])
 
     # list of properties names and type for filtered activities
     # should be retrieved before filtering by measurement name
-    metric_data['fields'] = list(measurements.values('name', 'type').distinct().order_by('type', 'name'))
+    # metric_data['fields'] = list(measurements.values('name', 'type').distinct().order_by('type', 'name'))
+    metric_data_fields_qs = measurements_qs.values('name', 'type').distinct().order_by('type', 'name')
 
-    return measurements
+    return measurements_qs, metric_data_fields_qs
 
 
+# for Raw metric
 def apply_filters(measurements_qs, filters, field_name):
+    """
+    Returns new QuerySet instance with filters from metric info applied to the provided QuerySet.
+
+    :param measurements_qs: QuerySet
+    :param filters: dict of filters from metric info
+    :param field_name: string - activity field name from metric info
+    :return: new QuerySet with applied filters
+    """
     group = filters.get('group', None)
     if group and int(group) >= 0:
         measurements_qs = measurements_qs.filter(activity__entity__group_id=group)
@@ -88,10 +123,11 @@ def apply_filters(measurements_qs, filters, field_name):
     return measurements_qs
 
 
+# for Composite metric
 def retrieve_composite_metric_data(metric, participation, metric_data, result_list):
     component_ids = list(map(int, metric.info['components']))
     aggregate = metric.info['aggregate']
-    groupby = metric.info.get('groupby', None)
+    group_by = metric.info.get('groupby', None)
 
     # TODO more than two components
     # find metrics if they already retrieved, otherwise put ids
@@ -114,7 +150,7 @@ def retrieve_composite_metric_data(metric, participation, metric_data, result_li
         activity_measurements = defaultdict(list)
         for comp in components:
             for idx, measurement in enumerate(comp['measurements']):
-                if groupby:
+                if group_by:
                     activity_measurements[measurement['activity_id']].append(measurement)
                 else:
                     activity_measurements[idx].append(measurement)
@@ -151,21 +187,21 @@ def retrieve_composite_metric_data(metric, participation, metric_data, result_li
         x_val = []
         y_val = []
 
-        if groupby:
-            if groupby['group_func'] == 'sum':
+        if group_by:
+            if group_by['group_func'] == 'sum':
                 agg_func = lambda a, b: a + b
-            elif groupby['group_func'] == 'count':
+            elif group_by['group_func'] == 'count':
                 agg_func = lambda a, b: a + 1
-            if groupby['group_func'] == 'min':
+            if group_by['group_func'] == 'min':
                 agg_func = min
-            if groupby['group_func'] == 'max':
+            if group_by['group_func'] == 'max':
                 agg_func = max
 
             def get_timestamp_from_activity(activity_value):
                 timestamp = 0
                 # TODO for any field of an activity
                 for m in activity_value['source']:
-                    if m['name'] == groupby['group_timefield']:
+                    if m['name'] == group_by['group_timefield']:
                         if m['type'] == "long":
                             timestamp = int(m['value']) / 1000  # without millis
                         elif m['type'] == "datetime":
@@ -175,13 +211,13 @@ def retrieve_composite_metric_data(metric, participation, metric_data, result_li
                 return int(timestamp)
 
             grouped = {}
-            if groupby['group_type'] == 'day':
+            if group_by['group_type'] == 'day':
                 grouped = group_by_day(activity_values, get_timestamp_from_activity, agg_func)
-            elif groupby['group_type'] == '3_days':
+            elif group_by['group_type'] == '3_days':
                 grouped = group_by_3_days(activity_values, get_timestamp_from_activity, agg_func)
-            elif groupby['group_type'] == '7_days':
+            elif group_by['group_type'] == '7_days':
                 grouped = group_by_7_days(activity_values, get_timestamp_from_activity, agg_func)
-            elif groupby['group_type'] == '30_days':
+            elif group_by['group_type'] == '30_days':
                 grouped = group_by_30_days(activity_values, get_timestamp_from_activity, agg_func)
 
             grouped_items = list(grouped.items())
@@ -283,15 +319,34 @@ def group_by_30_days(activity_values, key_func, agg_func):
 # retrieving only current metrics values
 
 def retrieve_current_metrics(participation):
+    """
+    Returns metrics with current values for composite metrics.
+    Also metrics contain list of available activity fields for that metric.
+
+    :param participation: UserParticipation model instance
+    :return: list of metrics with values and available fields
+    """
     result = []
     metrics = Metric.objects.filter(participation=participation)
     for m in metrics:
         result.append(retrieve_current_metric_data(m, participation))
-    result.sort(key=lambda m: m['id'])
+    result.sort(key=lambda metric: metric['id'])
     return result
 
 
-def retrieve_current_metric_data(metric, participation, strategy="NO_RAW", groupby=None):
+def retrieve_current_metric_data(metric, participation, strategy="NO_RAW", group_by=None):
+    """
+    Returns metrics with current values for composite metrics.
+
+    :param metric: metric id or Metric model instance
+    :param participation: UserParticipation model instance
+    :param strategy: "NO_RAW" - returns only available fields without metric data for raw metrics,
+    "LAST" - returns last measurement value (by id) for raw metric,
+    "GROUPING" - groups and aggregates measurements values according to 'group_by' info.
+
+    :param group_by: groupby dict from metric info
+    :return: metric dict with fields and values
+    """
     if type(metric) is int:
         metric = Metric.objects.get(pk=metric)
         metric_data = model_to_dict(metric)
@@ -299,68 +354,134 @@ def retrieve_current_metric_data(metric, participation, strategy="NO_RAW", group
         metric_data = model_to_dict(metric)
 
     if metric.type == Metric.COMPOSITE:
-        retrieve_current_composite_metric_data(metric, participation, metric_data)
+        metric_data['value'] = retrieve_current_composite_metric_value(metric, participation)
     elif strategy != "NO_RAW":
-        retrieve_current_raw_metric_data(metric, participation, metric_data, strategy, groupby)
+        # 'LAST' or 'GROUPING'
+        metric_data['value'], fields_qs = retrieve_current_raw_metric_value(metric, participation, strategy, group_by)
     else:
         # for retrieving only available activities fields
-        raw_metric_filters_qs(metric, participation, metric_data)
+        _, fields_qs = raw_metric_filters_qs(metric, participation)
+
+    if (metric.type == Metric.RAW) and (strategy != "LAST") and (strategy != "GROUPED"):
+        metric_data['fields'] = list(fields_qs)
 
     return metric_data
 
 
-def retrieve_current_raw_metric_data(metric, participation, metric_data, strategy="LAST", groupby=None):
-    measurements = raw_metric_filters_qs(metric, participation, metric_data)
-    measurements = measurements.filter(name=metric.info['field'])
+def retrieve_current_raw_metric_value(metric, participation, strategy="LAST", group_by=None):
+    """
+    Returns current value of a raw metric according to specified strategy.
 
+    :param metric: Metric model instance
+    :param participation: UserParticipation model instance
+    :param strategy: "LAST" - returns last measurement value (by id) for raw metric,
+    "GROUPING" - groups and aggregates measurements values according to 'group_by' info.
+
+    :param group_by: groupby dict from metric info
+    :return: (metric_value, metric_fields) tuple of metric value and available activity fields QuerySet
+    """
+    if metric.type != Metric.RAW:
+        raise ValueError
+
+    measurements_qs, fields_qs = raw_metric_filters_qs(metric, participation)
+    measurements_qs = measurements_qs.filter(name=metric.info['field'])
+
+    value = None
     if strategy == "LAST":
-        metric_data['value'] = measurements.order_by('-id').first().value
-
+        value = int(measurements_qs.order_by('-id').first().value)
     elif strategy == "GROUPING":
-        metric_data['value'] = 0
-        activity_ids = measurements.values_list('activity_id', flat=True)
+        value = retrieve_grouped_current_raw_metric_value(measurements_qs, group_by)
 
-        if groupby['group_type'] == 'day':
-            cur_day_timestamp = int(time.time() / (24 * 60 * 60)) * 24 * 60 * 60
-            time_measurements = Measurement.objects.filter(activity__in=activity_ids, name=groupby['group_timefield'])
-
-            allowed_activities = set()
-            for t in time_measurements:
-                if t.type == "long":
-                    timestamp = int(t.value) / 1000  # without millis
-                elif t.type == "datetime":
-                    timestamp = dateutil.parser.parse(t.value.upper()).timestamp()
-                else:
-                    continue
-
-                if timestamp >= cur_day_timestamp:
-                    allowed_activities.add(t.activity_id)
-
-            measurements = measurements.filter(activity__in=allowed_activities)
-
-        values = measurements.values_list('type', 'value')
-
-        if groupby['group_func'] == 'count':
-            metric_data['value'] = len(values)
-        elif groupby['group_func'] == 'sum':
-            metric_data['value'] = 0
-            for v in values:
-                if (v[0] == "long") or (v[0] == "int"):
-                    metric_data['value'] += int(v[1])
-                elif v[0] == "datetime":
-                    metric_data['value'] += dateutil.parser.parse(v[1].upper()).timestamp() * 1000  # with millis
+    return value, fields_qs
 
 
-def retrieve_current_composite_metric_data(metric, participation, metric_data):
+def retrieve_grouped_current_raw_metric_value(measurements_qs, group_by):
+    """
+    Returns current metric value using grouping operation.
+
+    :param measurements_qs: QuerySet instance for filtered measurements
+    :param group_by: groupby dict from metric info
+    :return: single integer value
+    """
+    if not group_by:
+        raise ValueError
+
+    activity_ids = measurements_qs.values_list('activity_id', flat=True)
+
+    if group_by['group_type'] == 'day':
+        day_shift = 0
+    elif group_by['group_type'] == '3_days':
+        day_shift = 2
+    elif group_by['group_type'] == '7_days':
+        day_shift = 6
+    elif group_by['group_type'] == '30_days':
+        day_shift = 29
+
+    period_start_sec = int(time.time() / DAY_SEC - day_shift) * DAY_SEC
+
+    time_measurements = Measurement.objects.filter(activity__in=activity_ids, name=group_by['group_timefield'])
+
+    allowed_activities = set()
+    for t in time_measurements:
+        if t.type == "long":
+            timestamp = int(t.value) / 1000  # without millis
+        elif t.type == "datetime":
+            timestamp = dateutil.parser.parse(t.value.upper()).timestamp()
+        else:
+            continue
+
+        if timestamp >= period_start_sec:
+            allowed_activities.add(t.activity_id)
+
+    measurements_qs = measurements_qs.filter(activity__in=allowed_activities)
+    measurements = measurements_qs.values('type', 'value')
+
+    fn = group_by['group_func']
+    if fn == 'count':
+        return measurements.count()
+
+    elif (fn == 'sum') or (fn == 'max') or (fn == 'min'):
+        # converting to numbers
+        values = []
+        for m in measurements:
+            if (m['type'] == "long") or (m['type'] == "int"):
+                values.append(int(m['value']))
+            elif m['type'] == "datetime":
+                values.append(dateutil.parser.parse(m['value'].upper()).timestamp() * 1000)  # with millis
+
+        # aggregating
+        if fn == 'sum':
+            aggr = sum
+        elif fn == 'max':
+            aggr = max
+        elif fn == 'min':
+            aggr = min
+
+        return aggr(values) if values else None
+
+    return None
+
+
+def retrieve_current_composite_metric_value(metric, participation):
+    """
+    Returns current composite metric value.
+
+    :param metric: Metric model instance
+    :param participation: UserParticipation model instance
+    :return: current composite metric value
+    """
+    if metric.type != Metric.COMPOSITE:
+        raise ValueError
+
     component_ids = list(map(int, metric.info['components']))
     aggregate = metric.info['aggregate']
-    groupby = metric.info.get('groupby', None)
+    group_by = metric.info.get('groupby', None)
 
-    if groupby:
+    if group_by:
         # TODO more than two components
         components = [
-            retrieve_current_metric_data(component_ids[0], participation, strategy="GROUPING", groupby=groupby),
-            retrieve_current_metric_data(component_ids[1], participation, strategy="GROUPING", groupby=groupby),
+            retrieve_current_metric_data(component_ids[0], participation, strategy="GROUPING", group_by=group_by),
+            retrieve_current_metric_data(component_ids[1], participation, strategy="GROUPING", group_by=group_by),
         ]
     else:
         components = [
@@ -369,29 +490,40 @@ def retrieve_current_composite_metric_data(metric, participation, metric_data):
         ]
 
     values = [c['value'] for c in components]
+    if None in values:
+        return None
 
+    val = None
     if aggregate == "minus":
-        metric_data['value'] = float(values[0]) - float(values[1])
+        val = values[0] - values[1]
     elif aggregate == 'timeinter':
-        # represent time as timestamp in seconds
-        c = int(dateutil.parser.parse(values[0].upper()).timestamp())
-        d = int(dateutil.parser.parse(values[1].upper()).timestamp())
-        metric_data['value'] = abs(c - d)
+        # already converted to seconds in the retrieve_current_metric_data()
+        val = abs(values[0] - values[1])
     elif aggregate == "sum":
-        metric_data['value'] = sum(values)
+        val = sum(values)
     elif aggregate == "mult":
-        metric_data['value'] = reduce(lambda x, y: x * y, values)
+        val = reduce(lambda x, y: x * y, values)
     elif aggregate == "div":
-        metric_data['value'] = float(values[0]) / float(values[1])
+        val = values[0] / values[1]
     elif aggregate == 'avg':
-        metric_data['value'] = sum(values) / float(len(values))
+        val = sum(values) / len(values)
     elif aggregate == 'min':
-        metric_data['value'] = min(float(values[0]), float(values[1]))
+        val = min(values)
     elif aggregate == 'max':
-        metric_data['value'] = max(float(values[0]), float(values[1]))
+        val = max(values)
 
+    return val
+
+
+######################################################################
 
 def get_activity_properties(participation):
+    """
+    Returns activities and their properties with types.
+
+    :param participation: UserParticipation model instance
+    :return: list of activities (dict with keys: 'name', 'properties'. A property is a dict with keys: 'name', 'type').
+    """
     lst = Entity.objects.filter(activity__participation=participation) \
         .annotate(activityName=F('name'),
                   propertyName=F('activity__measurements__name'),
